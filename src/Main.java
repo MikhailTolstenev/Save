3dd95249b6289bcc4db397736a4b8849d1780bbd
Mikhail Tolstenev <Tolstenev96@gmail.com>
2023-08-24 17:58:42 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,77 @@
 import java.io.*;
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
 
-    public static void saveGame (String adres, GameProgress save) {
-        try (FileOutputStream fos = new FileOutputStream(adres) ;
-             ObjectOutputStream oos = new ObjectOutputStream(fos))
-        {
+    public static void saveGame(String adres, GameProgress save) {
+        try (FileOutputStream fos = new FileOutputStream(adres);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(save);
-        } catch ( Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
     }
-    public static void zipFiles(String adres, String file1, String file2, String file3){
-        try(ZipOutputStream zout = new ZipOutputStream(new
-                FileOutputStream(adres));
-            FileInputStream fis1 = new FileInputStream(file1);
-            FileInputStream fis2 = new FileInputStream(file2);
-            FileInputStream fis3 = new FileInputStream(file3))
-        {
-            ZipEntry entry1 = new ZipEntry("newsave1.dat");
-            ZipEntry entry2 = new ZipEntry("newsave2.dat");
-            ZipEntry entry3 = new ZipEntry("newsave3.dat");
-            zout.putNextEntry(entry1);
-            zout.putNextEntry(entry2);
-            zout.putNextEntry(entry3);
-            byte[] buffer =new byte[fis1.available()];
-            fis1.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
-            byte[] buffer2 =new byte[fis2.available()];
-            fis2.read(buffer2);
-            zout.write(buffer2);
-            zout.closeEntry();
-            byte[] buffer3 =new byte[fis3.available()];
-            fis3.read(buffer3);
-            zout.write(buffer3);
-            zout.closeEntry();
 
-        } catch (Exception ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-    public static void delete(String adres){
-        File dir = new File(adres);
-        if (dir.delete())
+    public static void delete(String adres11) {
+        File dir = new File(adres11);
+        if (dir.delete()) {
             System.out.println("Файл удален");
+        } else System.out.println("Не обнаружено");
     }
+
+    public static void zipFiles(String adres, ArrayList<String> Files) {
+        int num = 1;
+        int BUFFER = 80000;
+
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(adres))) {
+
+            for (String file : Files) {
+                processFile(zout, file);
+            }
+            zout.closeEntry();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void processFile(ZipOutputStream out, String file1) {
+        try (FileInputStream fis = new FileInputStream(file1)) {
+            ZipEntry entry = new ZipEntry(String.valueOf(file1.hashCode()));
+            out.putNextEntry(entry);
+
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+
+            out.write(buffer);
+
+            out.closeEntry();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+
+        }
+    }
+
     public static void main(String[] args) {
-        GameProgress save1=new GameProgress(10,10,0,1);
-        GameProgress save2=new GameProgress(15,8,2,20);
-        GameProgress save3=new GameProgress(1,3,5,30);
-//        C:\Games\savegame
+        GameProgress save1 = new GameProgress(10, 10, 0, 1);
+        GameProgress save2 = new GameProgress(15, 8, 2, 20);
+        GameProgress save3 = new GameProgress(1, 3, 5, 30);
+
         saveGame("C:\\Games\\savegame\\save1.dat", save1);
         saveGame("C:\\Games\\savegame\\save2.dat", save2);
         saveGame("C:\\Games\\savegame\\save3.dat", save3);
+        ArrayList<String> files = new ArrayList<>();
+        files.add("C:\\Games\\savegame\\save1.dat");
+        files.add("C:\\Games\\savegame\\save2.dat");
+        files.add("C:\\Games\\savegame\\save3.dat");
 
-        zipFiles("C:\\Games\\savegame\\saves.zip",
-                "C:\\Games\\savegame\\save1.dat",
-                "C:\\Games\\savegame\\save2.dat",
-                "C:\\Games\\savegame\\save3.dat");
+
+        zipFiles("C:\\Games\\savegame\\saves11.zip", files);
+
         delete("C:\\Games\\savegame\\save1.dat");
         delete("C:\\Games\\savegame\\save2.dat");
         delete("C:\\Games\\savegame\\save3.dat");
